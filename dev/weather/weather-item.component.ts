7ddee7987e33d1a33432f40bb5b97150d7346e39
Maximilian Schwarzmüller <mblacky0@gmail.com>
2016-03-24 09:04:18 +0100

    Finished video on custom classes and string interpolation

--- dev/weather/weather-item.component.ts
+++ dev/weather/weather-item.component.ts
@@ -1,20 +1,25 @@
 import {Component} from 'angular2/core';
+import {WeatherItem} from "./weather-item";
 
 @Component({
     selector: 'weather-item',
     template: `
         <article class="weather-element">
             <div class="col-1">
-                <h3>Cityname</h3>
-                <p class="info">CLOUDS</p>
+                <h3>{{ weatherItem.cityName }}</h3>
+                <p class="info">{{ weatherItem.description }}</p>
             </div>
             <div class="col-2">
-                <span class="temperature">32°C</span>
+                <span class="temperature">{{ weatherItem.temperature }}°C</span>
             </div>
         </article>
     `,
     styleUrls: ['src/css/weather-item.css']
 })
 export class WeatherItemComponent {
-    
+    weatherItem: WeatherItem;
+
+    constructor() {
+        this.weatherItem = new WeatherItem('London', 'RAINY', 4);
+    }
 }
---
+++ dev/weather/weather-item.ts
@@ -0,0 +1,3 @@
+export class WeatherItem {
+    constructor(public cityName: string, public description: string, public temperature: number) {}
+}
